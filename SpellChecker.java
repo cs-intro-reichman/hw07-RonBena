737c3bb96a76660dbaa1239c5b09230c42f9050c
Ron Ben-Aharon <ronbena@MacBook-Pro-Ron.local>
2024-02-07 18:41:08 +0200

    lev

--- SpellChecker.java
+++ SpellChecker.java
@@ -11,11 +11,28 @@ public class SpellChecker {
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+	str = str.substring(1);
+	return str;
 	}
-
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		if(word1.length()==0)
+			return word2.length();
+			if(word2.length()==0)
+			return word1.length();
+
+		word1.toLowerCase();
+		word2.toLowerCase();
+			int dis = 0;
+			if(word1.charAt(0)== word2.charAt(0) )
+			{
+				return levenshtein(tail(word1), tail(word2));
+			}
+			else if(word1.charAt(0)!= word2.charAt(0))
+			{
+				dis++;
+				dis = dis + Math.min(levenshtein(tail(word1), tail(word2)),Math.min(levenshtein((word1), tail(word2)),levenshtein(tail(word1), tail(word2))));
+			}
+			return dis;
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -23,13 +40,14 @@ public class SpellChecker {
 
 		In in = new In(fileName);
 
-		// Your code here
-
+		for(int i=0; i < dictionary.length; i++)
+		dictionary[i] =  in.readLine();
+		
 		return dictionary;
 	}
 
-	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
-	}
+	// public static String spellChecker(String word, int threshold, String[] dictionary) {
+	
+	// }
 
 }
